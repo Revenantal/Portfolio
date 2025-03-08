ebb237d71fb7a18762cc6b69294a7fe59a421a47
Adrian <adrian@rocket-marketing.ca>
2025-03-08 16:39:29 -0500

    Correcting an infinite loop

--- components/console/logWindow.tsx
+++ components/console/logWindow.tsx
@@ -1,22 +1,17 @@
 "use client";
 
 import { usePathname } from 'next/navigation'
-import { useEffect, useCallback } from 'react';
+import { useEffect } from 'react';
 import { useConsoleTray } from './console-tray-provider';
 
 
 export default function LogWindow() {
-  const { log, setLog } = useConsoleTray();
+  const { log, setLog } = useConsoleTray() as { log: string[], setLog: React.Dispatch<React.SetStateAction<string[]>> };
   const pathname = usePathname();
 
-  const updateLog = useCallback(() => {
-    setLog([...log, pathname]);
-  }, [setLog, log, pathname]);
-
   useEffect(() => {
-    updateLog();
-  }, [updateLog])
-
+    setLog((prevLog: string[]) => [...prevLog, pathname]);
+  }, [pathname, setLog]);
 
   return (
     <div className="text-sm px-3 overflow-y-auto flex flex-col-reverse custom-scrollbar">
